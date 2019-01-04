b55d66524eb76b825af668140890c90aa70bc9ec
HO KHENG YEE <ho.kheng.yee@gmail.com>
2019-01-05 01:46:44 +0800

    Homework Assignment #1 checkpoint

---
+++ config.js
@@ -0,0 +1,25 @@
+/* Create and export configuration variables */
+
+// Container for all the environments
+const environments = {};
+
+// Staging (default) environment
+environments.staging = {
+    'port': 3000,
+    'envName': 'staging'
+};
+
+// Production environment
+environments.production = {
+    'port': 8080,
+    'envName': 'production'
+};
+
+// Determine which environment was passed as a command-line argument
+const currentEnvironment = typeof (process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+// Check that the current environment exist, if not, default to staging
+const environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+// Exports the module
+module.exports = environmentToExport;
